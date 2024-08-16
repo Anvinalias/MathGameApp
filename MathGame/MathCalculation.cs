b33c06db35a134f527ff9dfca2bd8bfb0878220f
Anvinalias <anvinkaippillil123@gmail.com>
2024-08-16 18:01:33 +0530

    Add CreateQuestion method

--- MathGame/MathCalculation.cs
+++ MathGame/MathCalculation.cs
@@ -11,5 +11,30 @@ namespace MathGame
             num1 = ran.Next(0, 100);
             num2 = ran.Next(0, 100);
         }
+
+        public void CreateQuestion(string choice)
+        {
+            switch (choice)
+            {
+                case "1":
+                    Addition(num1, num2);
+                    break;
+                case "2":
+                    Substraction(num1, num2);
+                    break;
+                case "3":
+                    Multiplication(num1, num2);
+                    break;
+                case "4":
+                    Division(num1, num2);
+                    break;
+                case "5":
+                    DisplayHistory();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+        }
     }
 }
