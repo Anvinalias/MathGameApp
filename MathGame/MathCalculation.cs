73d4534493a30e7bf6a4030d00a318e4e047c4bb
Anvinalias <anvinkaippillil123@gmail.com>
2024-08-16 17:39:20 +0530

    Add a method CreateOperands to random generate operands for the game

--- MathGame/MathCalculation.cs
+++ MathGame/MathCalculation.cs
@@ -1,9 +1,19 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MathGame
 {
     internal class MathCalculation
     {
-
+        int num1, num2;
+        Random ran = new Random();
+        public void CreateOperands()
+        {
+            num1 = ran.Next(0, 100);
+            num2 = ran.Next(0, 100);
+        }
     }
 }
