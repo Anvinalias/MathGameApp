0d93cec2bc6d74418b37f927f875d375a35a373f
Anvinalias <anvinkaippillil123@gmail.com>
2024-08-16 17:30:15 +0530

    Add player choice and let the user choice wheather to continue

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿namespace MathGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? playerChoice;
+            string? replayChoice;
+
+            do
+            {
+                Console.WriteLine("Choose from below choices ?");
+
+                Console.WriteLine("1. Addition");
+                Console.WriteLine("2. Substraction");
+                Console.WriteLine("3. Multiplication");
+                Console.WriteLine("4. Division");
+                Console.WriteLine("5. History");
+                Console.WriteLine("6. Quit");
+
+                playerChoice = Console.ReadLine();
+
+                if (playerChoice == "6")
+                {
+                    break;
+                }
+
+                createOperands();
+
+                Console.WriteLine("Do you want to replay? Y or N ");
+                replayChoice = Console.ReadLine();
+
+            } while (replayChoice != "n" && replayChoice != "N");
+        }
+    }
+}
